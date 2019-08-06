e8af8e3fdcfa71d03ad3b9cc869a9b324927f359
Joe Lake <joelake123@gmail.com>
2019-08-06 16:41:20 -0500

    created basic components [section 3]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react'
+import FirstComponent from './components/learning-examples/FirstComponent'
+import SecondComponent from './components/learning-examples/SecondComponent'
+import ThirdComponent from './components/learning-examples/ThirdComponent'
+import Counter from './components/counter/Counter'
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter by={1}/>
+        <Counter by={5}/>
+        <Counter by={10}/>
+      </div>
+    )
+  }  
 }
 
-export default App;
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
+          My Hello World
+          <FirstComponent />
+          <SecondComponent />
+          <ThirdComponent />
+      </div>
+    )
+  }  
+} 
+
+export default App
