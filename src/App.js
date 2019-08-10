f3a900439c2f8059c0fe129689422d084baf70e3
Joe Lake <joelake123@gmail.com>
2019-08-10 17:12:51 -0500

    built sample todo app frontend

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import React, { Component } from 'react'
 // import SecondComponent from './components/learning-examples/SecondComponent'
 // import ThirdComponent from './components/learning-examples/ThirdComponent'
 import Counter from './components/counter/Counter'
+import TodoApp from './components/todo/TodoApp'
 import logo from './logo.svg';
 import './App.css'
+import './bootstrap.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="wrapper">
-          <h1>STL Foodtrucks</h1>
-          <Counter by={1}/>
-          <Counter by={5}/>
-          <Counter by={10}/>
-        </div>
+          {/* <Counter /> */}
+          <TodoApp/>
       </div>
     )
   }  
