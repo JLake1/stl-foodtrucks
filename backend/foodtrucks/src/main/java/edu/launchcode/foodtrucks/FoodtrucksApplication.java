d69493f8698a66be6a5b5f7f2ed5fc105e9741d6
Joe Lake <joelake123@gmail.com>
2019-08-10 19:42:41 -0500

    added backend directory, set up spring boot

---
+++ backend/foodtrucks/src/main/java/edu/launchcode/foodtrucks/FoodtrucksApplication.java
@@ -0,0 +1,13 @@
+package edu.launchcode.foodtrucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtrucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtrucksApplication.class, args);
+	}
+
+}
