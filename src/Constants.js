2e6168d774343d4a08edc3557ba7dec420aec7a3
Joe Lake <joelake123@gmail.com>
2020-02-19 15:39:48 -0600

    revert constants url test

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
 export const API_URL = 'http://localhost:8080'
-export const JPA_API_URL = 'http://localhost:8080/jpa'
+export const JPA_API_URL = process.env.NODE_ENV == "PROD" ? 'https://stl-foodtrucks.herokuapp.com' : 'http://localhost:8080/jpa'
