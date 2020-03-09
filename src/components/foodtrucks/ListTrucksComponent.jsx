088408a435b767b131d1e0d93f5dc569764cc982
Joe Lake <joelake123@gmail.com>
2020-03-09 18:29:30 -0500

    cleaned up code - ListTrucksComponent

--- src/components/foodtrucks/ListTrucksComponent.jsx
+++ src/components/foodtrucks/ListTrucksComponent.jsx
@@ -1,76 +1,48 @@
-import React, {Component} from 'react'
-import TruckDataService from '../../api/foodtrucks/TruckDataService.js'
-import AuthenticationService from './AuthenticationService.js'
-import HeaderComponent from './HeaderComponent'
-import moment from 'moment'
-import { userInfo } from 'os';
+import React, {Component} from 'react';
+import TruckDataService from '../../api/foodtrucks/TruckDataService.js';
+import AuthenticationService from './AuthenticationService.js';
+import HeaderComponent from './HeaderComponent';
+import moment from 'moment';
 
 class ListTrucksComponent extends Component {
     
     constructor(props){ 
-        super(props)
+        super(props);
         this.state = {
             trucks : [],
             message : null,
             userType : 'owner'
-        }
-
-        this.deleteTruckClicked = this.deleteTruckClicked.bind(this)
-        this.refreshTrucks = this.refreshTrucks.bind(this)
+        };
+        this.deleteTruckClicked = this.deleteTruckClicked.bind(this);
+        this.refreshTrucks = this.refreshTrucks.bind(this);
     }
  
-    shouldComponentUpdate(nextProps, nextState) {
-        return true
-    }
-
     componentDidMount() { 
         this.refreshTrucks()
-        // console.log(this.state)
     }
 
     refreshTrucks() {
-        let username = AuthenticationService.getLoggedInUserName()
+        let username = AuthenticationService.getLoggedInUserName();
         TruckDataService.retrieveAllTrucks(username)
           .then(
               response => {
-                  //console.log(response);
-                  this.setState({trucks : response.data})
+                  this.setState({trucks : response.data});
               }
-          ) 
+          );
     }
 
     deleteTruckClicked(id) {
-        let username = AuthenticationService.getLoggedInUserName()
-        // console.log(id + " " + username);
+        let username = AuthenticationService.getLoggedInUserName();
         TruckDataService.deleteTruck(username, id)
           .then (
               response => {
-                  this.setState({message : `Delete of truck ${id} Successful`})
-                  this.refreshTrucks()
+                  this.setState({message : `Delete of truck ${id} Successful`});
+                  this.refreshTrucks();
               }
-          )
+          );
     }
-
-    // updateTruckClicked(id) {
-    //     console.log('update' + id)  
-    //     this.props.history.push(`/trucks/${id}`)      
-    //     // let username = AuthenticationService.getLoggedInUserName()
-    //     // // console.log(id + " " + username);
-    //     // TruckDataService.deleteTruck(username, id)
-    //     //   .then (
-    //     //       response => {
-    //     //           this.setState({message : `Delete of truck ${id} Successful`})
-    //     //           this.refreshTrucks()
-    //     //       }
-    //     //   )
-    // }
-
-    // addTruckClicked(id) {
-    //     this.props.history.push(`/trucks/-1`)  
-    // }
     
     render() {
- 
         return (
             <div>
             <HeaderComponent></HeaderComponent> 
@@ -81,15 +53,11 @@ class ListTrucksComponent extends Component {
                 <div className="row">     
                     <div className="col-sm-12 owner-content">
                     <h1>My Trucks</h1> 
-                    
-                    {/* <p>{this.state.userType && <div>{this.state.userType}</div>}</p> */}
                     <table className="table table-hover" key="truck.id">
                         <thead className="thead-dark">
                             <tr> 
                                 <th>Truck Name</th>  
                                 <th>Next Event</th> 
-                                {/* <th>Update</th>
-                                <th>Delete</th> */}
                                 <th className="delete-col"></th>
                             </tr>
                         </thead>
@@ -97,8 +65,7 @@ class ListTrucksComponent extends Component {
                         {
                             this.state.trucks.map (    
                                 truck =>
-                                    <tr key={truck.id}>  
-                                        {console.log(truck, truck.id)}
+                                    <tr key={truck.id}>
                                         <td><a href={"/truck_profile/" + truck.urlTag}>{truck.description}</a></td>  
                                         <td>{moment(truck.targetDate).format('lll')}</td>   
                                         <td>
@@ -109,15 +76,13 @@ class ListTrucksComponent extends Component {
                         }
                         </tbody>
                     </table>
- 
                     </div>
- 
                     </div>
             </div>
             </div>
             </div>
-        )
+        );
     }
 }
 
-export default ListTrucksComponent
+export default ListTrucksComponent;
