b66b5a6f4a1f71ecb8193cb664633b4e756af76c
Joe Lake <joelake123@gmail.com>
2020-03-09 18:04:34 -0500

    cleaned up code - ErrorComponent

--- src/components/foodtrucks/ErrorComponent.jsx
+++ src/components/foodtrucks/ErrorComponent.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function ErrorComponent() {
-    return <div> An Error Occurred.</div>
+    return <div> An Error Occurred.</div>;
 }
 
-export default ErrorComponent
+export default ErrorComponent;
